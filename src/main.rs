1921d63c8c603021bf5ce07beb5b10bc31777ea9
Sean Hellum <seanhellum45@gmail.com>
2020-01-30 16:25:43 +0000

    snake_casify

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ fn main() {
                 Some(n) => String::from(".") + n.as_str(),
                 None => String::from(".mydsl"),
             };
-            let projectName = match f {
+            let project_name = match f {
                 Some(f) => f,
                 None => String::from("org.xtext.example.mydsl"),
             };
@@ -46,11 +46,11 @@ fn main() {
                 Ok(()) => {}
                 Err(_n) => println!("\x1b[1;31mError:\x1b[0m directory already exists"),
             }
-            create_dir(&n.join(&projectName)).unwrap();
-            create_dir(&n.join(projectName.to_owned() + &String::from(".ide"))).unwrap();
-            create_dir(&n.join(projectName.to_owned() + &String::from(".tests"))).unwrap();
-            create_dir(&n.join(projectName.to_owned() + &String::from(".ui"))).unwrap();
-            create_dir(&n.join(projectName.to_owned() + &String::from(".ui.tests"))).unwrap();
+            create_dir(&n.join(&project_name)).unwrap();
+            create_dir(&n.join(project_name.to_owned() + &String::from(".ide"))).unwrap();
+            create_dir(&n.join(project_name.to_owned() + &String::from(".tests"))).unwrap();
+            create_dir(&n.join(project_name.to_owned() + &String::from(".ui"))).unwrap();
+            create_dir(&n.join(project_name.to_owned() + &String::from(".ui.tests"))).unwrap();
             return;
         }
     }
